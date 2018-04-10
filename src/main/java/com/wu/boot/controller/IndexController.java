88ed0234b7a23ab27020f8a2990415eb18f626d8
wucaiqiang <349268692@qq.com>
2018-04-10 15:11:01 +0800

    aaa update

--- src/main/java/com/wu/boot/controller/IndexController.java
+++ src/main/java/com/wu/boot/controller/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
     @RequestMapping("/index")
     public String index() throws Exception {
         System.out.println("调用页面开始...");
-        String bbbb="bbbb";
+        String aaaa="aaaa";
         return "index";
     }
     @RequestMapping(value = "/test1",produces = "application/json;charset=UTF-8")
